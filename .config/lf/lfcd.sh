4b203eaa00e03b4c798157cae1256c251bfca0e4
66RING <1552087953@qq.com>
2021-04-05 21:12:37 +0800

    more smart lf

--- .config/lf/lfcd.sh
+++ .config/lf/lfcd.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 
 __lf() {
-  /home/ring/.config/lf/lfimg.sh "$@"
+  if [ -n "$DISPLAY" ]; then
+    export FIFO_UEBERZUG="${TMPDIR:-/tmp}/lf-ueberzug-$$"
+
+    cleanup() {
+      exec 3>&-
+      rm "$FIFO_UEBERZUG"
+    }
+
+    if [ -f "$FIFO_UEBERZUG" ]; then
+      rm "$FIFO_UEBERZUG"
+    fi
+    mkfifo "$FIFO_UEBERZUG"
+    ueberzug layer --silent <"$FIFO_UEBERZUG" &
+    upid=$!
+    exec 3>"$FIFO_UEBERZUG"
+    trap cleanup EXIT
+
+    if ! [ -d "$HOME/.cache/lf" ]; then
+      mkdir -p "$HOME/.cache/lf"
+    fi
+
+    lf "$@" 3>&-
+  else
+    lf "$@"
+  fi
 }
 
 lfcd () {
     tmp="$(mktemp)"
     export LF_BACK="$(pwd)"
-    __lf -last-dir-path="$tmp" "$@"
+    (__lf -last-dir-path="$tmp" "$@")
     if [ -f "$tmp" ]; then
         dir="$(cat "$tmp")"
         rm -f "$tmp"

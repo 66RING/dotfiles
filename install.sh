e0b34e3bac274a430596f12903e3cacb74a53908
66RING <1552087953@qq.com>
2021-08-04 17:09:35 +0800

    deprecated .sh suffix

--- install.sh
+++ install.sh
@@ -9,9 +9,7 @@ ln -s $path/nvim/init.vim $HOME/.vimrc
 # zsh
 ln -s $path/.config/zsh/.zprofile $HOME/.zprofile
 
-# bins
+# bins + applications
 ln -s $path/.local/bin/* $HOME/.local/bin/
-
-# applications
 ln -s $path/.local/share/applications/* $HOME/.local/share/applications/
 

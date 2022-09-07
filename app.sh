eda77408303d8b561fa68e198d83658bdb835240
66RING <travisturing@qq.com>
2022-09-08 04:28:14 +0800

    u

---
+++ app.sh
@@ -0,0 +1,9 @@
+yay -S nerd-fonts-cascadia-code \
+  cascaydia-code-fonts \
+  adobe-source-han-mono-cn-fonts \
+  adobe-source-han-mono-otc-fonts \
+  ttf-mac-fonts \
+  clash-for-windows-bin \
+  ttf-twemoji \
+  ttf-weather-icons \
+

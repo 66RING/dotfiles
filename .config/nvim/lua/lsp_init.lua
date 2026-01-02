67d521659d45f7b37a6c396807856106c47ff0b8
66RING <travisturing@qq.com>
2026-01-02 12:01:47 +0800

    u

--- .config/nvim/lua/lsp_init.lua
+++ .config/nvim/lua/lsp_init.lua
@@ -2,6 +2,7 @@ vim.lsp.enable("lua_ls")
 vim.lsp.enable("clangd")
 vim.lsp.enable("pyright")
 vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("gopls")
 
 vim.keymap.del("v", "in")
 vim.keymap.del("o", "in")
